59107914028d9d50e4a8d6631d8ac54db09488ad
Satya Ranjon <satyaranjonofficial@gmail.com>
2024-03-31 22:28:13 +0600

    create verifyinvitation, createTeamUser or saveActivityLogNotification actions

--- src/app/(main)/agency/page.tsx
+++ src/app/(main)/agency/page.tsx
@@ -1,13 +1,41 @@
-import { getAuthUserDetails } from "@/lib/actions";
+import { getAuthUserDetails, verifyAndAcceptInvitation } from "@/lib/actions";
 import { currentUser } from "@clerk/nextjs";
+import { Plan } from "@prisma/client";
 import { redirect } from "next/navigation";
-import React from "react";
 
-const Page = async () => {
-  const user = await currentUser();
-  if (!user) redirect("/agency/sign-in");
+const Page = async ({
+  searchParams,
+}: {
+  searchParams: { plan: Plan; state: "string"; code: string };
+}) => {
+  // const user = await currentUser();
+  // if (!user) redirect("/agency/sign-in");
 
-  const x = await getAuthUserDetails();
+  const agencyId = await verifyAndAcceptInvitation();
+  const user = await getAuthUserDetails();
+
+  if (agencyId) {
+    if (user?.role === "SUBACCOUNT_GUEST" || user?.role === "SUBACCOUNT_USER") {
+      return redirect("/subaccount");
+    }
+    if (user?.role === "AGENCY_OWNER" || user?.role === "AGENCY_ADMIN") {
+      if (searchParams.plan) {
+        return redirect(
+          `/agency/${agencyId}/billing?plan=${searchParams.plan}`
+        );
+      }
+      if (searchParams.state) {
+        const statePath = searchParams.state.split("__")[0];
+        const stateAgencyId = searchParams.state.split("__")[1];
+        if (!stateAgencyId) {
+          `/agency/${stateAgencyId}/${statePath}?code=${searchParams.code}`;
+        }
+      } else return redirect(`/agency/${agencyId}`);
+    } else {
+      return <>NOt authorized 404</>;
+    }
+  }
+  const authUser = await currentUser();
 
   return <div>Agency Page</div>;
 };
