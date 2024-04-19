28d906871bd27ceec92c167e5de311ec62a70cca
Satya Ranjon <satyaranjonofficial@gmail.com>
2024-04-19 23:25:44 +0600

    Change design

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -23,10 +23,10 @@ const Navigation = async ({ user }: Props) => {
       </aside>
       <nav className="hidden md:block absolute left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[-50%]">
         <ul className="flex items-center justify-center gap-8">
-          <Link href={"#"}>Pricing</Link>
-          <Link href={"#"}>About</Link>
-          <Link href={"#"}>Documentation</Link>
-          <Link href={"#"}>Features</Link>
+          <Link href={"/#"}>Pricing</Link>
+          <Link href={"/#"}>About</Link>
+          <Link href={"/#"}>Documentation</Link>
+          <Link href={"/#"}>Features</Link>
         </ul>
       </nav>
       <aside className="flex gap-2 items-center">
